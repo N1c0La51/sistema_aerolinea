f61e64519d92202b7c31e9e2c6c75ca2b378641c
N1c0La51 <ncardonad105680@umanizales.edu.co>
2024-03-10 16:57:50 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 const readlineSync = require('readline-sync');
 
+//varivalbes para mantener un registro del numero de maletas despachadas por destino
+let maletasSanAndres = 0;
+let maletasPereira = 0;
+let maletasMedellin = 0;
+let maletasCali = 0;
+let maletasManizales = 0;
+let maletasBarranquilla = 0;
+let maletasBogota = 0;
+
+function ProcesarMaleta(destino) {
+    switch (destino) {
+        case 'San Andres':
+            maletasSanAndres++;
+            break;
+        case 'Pereira':
+            maletasPereira++;
+            break;
+        case 'Medellin':
+            maletasMedellin++;
+            break
+        case 'Cali':
+            maletasCali++;
+            break;
+        case 'Manizales':
+            maletasManizales++;
+            break;
+        case 'Barranquilla':
+            maletasBrranquilla;
+            break;
+        case 'Bogota':
+            maletasBogota;
+            break;
+
+    }
+}
+//funcion para obtener el destino al que se despacharon mas maletas
+function obtenerDestinoMasDespachado() {
+    let DestinoMasDespachado = '';
+    let maxMaletas = -1;
+    if (maletasSanAndres > maxMaletas) {
+        DestinoMasDespachado = 'San Andres';
+        maxMaletas = maletasSanAndres;
+        if (maletasPereira > maxMaletas) {
+            DestinoMasDespachado = 'Pereira'
+            maxMaletas = maletasPereira;
+        }
+        if (maletasMedellin > maxMaletas) {
+            DestinoMasDespachado = 'Medellin'
+            maxMaletas = maletasMedellin;
+        }
+        if (maletasCali > maxMaletas) {
+            DestinoMasDespachado = 'Cali'
+            maxMaletas = maletasCali;
+        }
+        if (maletasManizales > maxMaletas) {
+            DestinoMasDespachado = 'Manizales'
+            maxMaletas = maletasManizales;
+        }
+        if (maletasBarranquilla > maxMaletas) {
+            DestinoMasDespachado = 'Barranquilla'
+            maxMaletas = maletasBarranquilla;
+        }
+        if (maletasBogota > maxMaletas) {
+            DestinoMasDespachado = 'Bogota'
+            maxMaletas = maletasBogota;
+        }
+        return DestinoMasDespachado;
+    }
+}
 function validarGenero(genero) {
     return genero.toLowerCase() === 'masculino' || genero.toLowerCase() === 'femenino';
 }
@@ -9,13 +78,11 @@ const PesoMaximoMaleta = 23; //en kilogramos
 const costoBaseMaleta = 20000; //costo base de una maleta que no excede el peso maximo
 const costoExtraPorKilo = 5000; //costo extra por cada kilogramo adicional
 
-
-
 function calcularCostoMaleta(pesoMaleta) {
     let costoTotalMaleta = costoBaseMaleta;
 
     if (pesoMaleta > PesoMaximoMaleta) {
-        const pesoExcedido = pesoMaleta - PesoMaximoMaleta;2
+        const pesoExcedido = pesoMaleta - PesoMaximoMaleta; 2
         const costoExtra = pesoExcedido * costoExtraPorKilo;
         costoTotalMaleta += costoExtra;
     }
@@ -51,6 +118,8 @@ function registrarMaleta() {
     const destino = readlineSync.question(`Ingrese el destino de su maleta: `);
 
     if (validarOrigen(destino)) {
+        //llamado a la funcion de procesar maleta
+        ProcesarMaleta(destino)
         //continuar con el registro de la maleta
         console.log(`Destino valido. Puede continuar con el registro de su maleta.`)
     } else {
@@ -80,3 +149,9 @@ function registrarMaleta() {
 
 // Ejemplo de uso
 registrarMaleta();
+registrarMaleta();
+registrarMaleta();
+registrarMaleta();
+
+const DestinoMasDespachado = obtenerDestinoMasDespachado();
+console.log(`El destino al que mas se despacharon maletas es: ${DestinoMasDespachado}`);
